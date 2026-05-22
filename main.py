562603d137e77d22b0fb8e98f1df9062582e50ad
Parsa SH <aparsash.dev@gmail.com>
2026-05-22 20:12:03 +0200

    display records dynamically on dashboard

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from app.database import Base, engine
-from app.models import User
+from sqlalchemy.orm import Session
+
+from app.database import (
+    Base,
+    engine,
+    get_db
+)
+
+from app.models import (
+    User,
+    Record
+)
+
 from app.auth import get_current_user
 
 from app.routers import (
@@ -36,6 +47,7 @@ app.include_router(records.router)
 @app.get("/")
 def home(
     request: Request,
+    db: Session = Depends(get_db),
     user: User = Depends(get_current_user)
 ):
     if not user:
@@ -45,10 +57,15 @@ def home(
             status_code=303
         )
 
+    all_records = db.query(Record).order_by(
+        Record.created_at.desc()
+    ).all()
+
     return templates.TemplateResponse(
         request=request,
         name="index.html",
         context={
-            "user": user
+            "user": user,
+            "records": all_records
         }
     )
